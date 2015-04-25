ac346e36a9dc24f4e2359ec593b7df2e679ddedf
kkk987 <markying21549@gmail.com>
2015-04-26 09:52:19 +1000

    Revert "Revert "Revert "Revert "Revert "Revert "ENGG4810 LCD display""""""
    This reverts commit 6b36ca9600fcfb7c8eec4b02f0db3059c8932466.

---
+++ Debug/ST7735TestMain.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+ST7735TestMain.obj: ../ST7735TestMain.c
+ST7735TestMain.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdio.h
+ST7735TestMain.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+ST7735TestMain.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+ST7735TestMain.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+ST7735TestMain.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdlib.h
+ST7735TestMain.obj: ../ST7735.h
+ST7735TestMain.obj: ../PLL.h
+ST7735TestMain.obj: C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h
+ST7735TestMain.obj: ../Switch.h
+
+../ST7735TestMain.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdlib.h: 
+../ST7735.h: 
+../PLL.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h: 
+../Switch.h: 
---
+++ Debug/Switch.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+Switch.obj: ../Switch.c
+Switch.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+Switch.obj: C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h
+
+../Switch.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h: 
