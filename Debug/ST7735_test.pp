12120c891eb83845cd61e0585c7d6a312b8495f2
kkk987 <markying21549@gmail.com>
2015-04-26 09:43:38 +1000

    ENGG4810 LCD display
    Tiva C TM4C123GXL and ST7735 LCD screen

---
+++ Debug/ST7735_test.pp
@@ -0,0 +1,5 @@
+# FIXED
+
+ST7735_test.obj: ../ST7735_test.c
+
+../ST7735_test.c: 
---
+++ Debug/widget.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+widget.obj: ../widget.c
+widget.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+widget.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+widget.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+widget.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+widget.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+widget.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+widget.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/debug.h
+widget.obj: ../grlib.h
+widget.obj: ../widget.h
+
+../widget.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/debug.h: 
+../grlib.h: 
+../widget.h: 
