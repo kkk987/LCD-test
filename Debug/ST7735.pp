18beb65fb0b5e2f78791a62bbe124d85fb68e542
kkk987 <markying21549@gmail.com>
2015-04-26 09:51:37 +1000

    Revert "Revert "Revert "Revert "ENGG4810 LCD display""""
    This reverts commit 084392fec758059cd4cdf5da4c8fd4a612f376e5.

---
+++ Debug/ST7735.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+ST7735.obj: ../ST7735.c
+ST7735.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdio.h
+ST7735.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+ST7735.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+ST7735.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+ST7735.obj: ../ST7735.h
+ST7735.obj: C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h
+
+../ST7735.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+../ST7735.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/tm4c123gh6pm.h: 
